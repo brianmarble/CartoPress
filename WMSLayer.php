1616b93d949a427e66b0f781c4a1a24d2a61b9b4
Alex Wells <alexwells@landez.com>
2013-03-06 11:44:27 -0700

    Working wms layer

--- WMSLayer.php
+++ WMSLayer.php
@@ -2,24 +2,25 @@
 
 class WMSLayer extends Layer{
 	
+	private $filename;
+	private $pageLayout;
+	
 	public function __construct($layer,$pageLayout,$bounds){
 		parent::__construct($layer);
+		$this->pageLayout = $pageLayout;
+		$layer->params->FORMAT = 'image/png';
 		$imageDealer = new ImageDealer($layer->url,$layer->params);
-		$imageDealer->addImage(array(
+		$this->filename = $imageDealer->addImage(array(
 			'WIDTH' => $pageLayout->getMapWidth('pixel'),
-			'HEIGHT' => $pageLayout->getMapWidth('pixel'),
+			'HEIGHT' => $pageLayout->getMapHeight('pixel'),
 			'BBOX' => "$bounds->left,$bounds->bottom,$bounds->right,$bounds->top"
 		));
-		var_dump($bounds,array(
-			'WIDTH' => $pageLayout->getMapWidth('pixel'),
-			'HEIGHT' => $pageLayout->getMapWidth('pixel'),
-			'BBOX' => "$bounds->left,$bounds->bottom,$bounds->right,$bounds->top"
-		));
-		die();
+		$imageDealer->getImages();
 	}
 
 	protected function drawLayer($pdf){
-		
+		$pageLayout = $this->pageLayout;
+		$pdf->Image($this->filename,1,1,$pageLayout->getMapWidth('in'),$pageLayout->getMapHeight('in'));
 	}
 }
 
