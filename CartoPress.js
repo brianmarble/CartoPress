3cad97ea255183fa245202a3b372eefd93a14a26
Alex Wells <1walexw@gmail.com>
2013-05-20 20:38:27 -0600

    Added shim for Array.prototype.forEach

--- CartoPress.js
+++ CartoPress.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// forEach shim
+if ( !Array.prototype.forEach ) {
+  Array.prototype.forEach = function(fn, scope) {
+    for(var i = 0, len = this.length; i < len; ++i) {
+      fn.call(scope, this[i], i, this);
+    }
+  }
+}
+
 var CartoPress = function(map,url){
 	this.url = url || CartoPress.serverUrl;
 	this.request = OpenLayers.Request.XMLHttpRequest;
