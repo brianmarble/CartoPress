bbf1e28d48c966c10b731eae984246314c652646
Alex Wells <1walexw@gmail.com>
2013-06-05 20:53:33 -0600

    increment using nextversion file

--- build/build.php
+++ build/build.php
@@ -1,8 +1,17 @@
 #!/usr/bin/php
 <?
 
-$version = date("Y.m.d.H.i.s");
 chdir(dirname(__FILE__));
+
+if(file_exists("nextversion.txt")){
+	$version = trim(file_get_contents("nextversion.txt"));
+	$nextVersion = explode('.',$version);
+	$nextVersion[count($nextVersion)-1] += 1;
+	$nextVersion = implode('.',$nextVersion);
+	file_put_contents("nextversion.txt",$nextVersion);
+} else {
+	$version = date("Y.m.d.H.i.s");
+}
 
 `rm -rf CartoPress`;
 `mkdir CartoPress`;
