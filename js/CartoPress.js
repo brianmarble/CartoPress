7c1bf6ef049480a5c860860b80156323137dcfe6
Alex Wells <1walexw@gmail.com>
2013-06-08 11:28:51 -0600

    Use uuids for pdf ids

--- js/CartoPress.js
+++ js/CartoPress.js
@@ -171,11 +171,17 @@ var CartoPress = OpenLayers.Class({
 	print: function(callback){
 		var bounds = this.selectPrintAreaControl.getBounds();
 		this.selectPrintAreaControl.deactivate();
-		var name = "CartoPress_"+(new Date().getTime());
+		var name = this.uuid();//"CartoPress_"+(new Date().getTime());
 		var map = this.selectPrintAreaControl.map;
-		cp.createPdf(map,bounds,this.currentLayout,name,callback);
+		this.createPdf(map,bounds,this.currentLayout,name,callback);
 	},
 	
+	uuid: function(){
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+			return v.toString(16);
+		});
+	},
 	
 	CLASS_NAME: "CartoPress"
 });
