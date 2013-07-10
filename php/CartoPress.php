60458147634fdeee03daf5709104f39041c35d99
Alex Wells <alexwells@landez.com>
2013-07-09 18:50:51 -0600

    wip

--- php/CartoPress.php
+++ php/CartoPress.php
@@ -6,7 +6,7 @@
  * 
  * GET  /                 retrieve information about this instance of cartopress
  * GET  /formats          list of formats
- * POST /pdfs/create      create a pdf
+ * POST /pdfs/{id}        create a pdf with the given id
  * GET  /pdfs/{id}        retrieve the pdf with the given id
  */
  
@@ -22,10 +22,8 @@ class CartoPress {
 			$this->outputInfo();
 		} else if ($request[0] == 'formats' && count($request) == 1 ){
 			$this->outputFormatList();
-		} else if ($request[0] == 'pdfs' && $request[1] == "create" && count($request) == 2){
-			$this->createPdf($request);
 		} else if ($request[0] == 'pdfs' && count($request) == 2){
-			$this->returnPdf($request);
+			$this->handlePdf($request);
 		} else {
 			if(false)http_response_code(404);
 			else {
@@ -51,51 +49,41 @@ class CartoPress {
 		
 	}
 	
-	private function createPdf($request){
-		$method = $_SERVER['REQUEST_METHOD'];
-		$pdfId = uniqid('CartoPress_',true);
-		$filename = Config::getInstance()->pdfDir."/".$pdfId;
-		if($method != 'POST'){
-			http_response_code(405);
-			header("Allow: POST");
-			die();
-		}
-		if($_SERVER['CONTENT_TYPE'] != 'application/json'){
-			http_response_code(415);
-			die();
-		}
-		
-		$data = json_decode(file_get_contents('php://input'));
-		$pdf = new PDFBuilder($data);
-		if($pdf && $pdf->saveTo($filename)){
-			http_response_code(201);
-			header("Content-type: application/json");
-			echo json_encode(array("url"=>$pdfId));
-		} else {
-			header("Content-type: application/json");
-			echo json_encode(array("success"=>false));
-		}
-	}
-	
-	private function returnPdf($request){
+	private function handlePdf($request){
 		$method = $_SERVER['REQUEST_METHOD'];
 		$filename = Config::getInstance()->pdfDir."/".$request[1];
-		if($method != 'GET'){
-			http_response_code(405);
-			header("Allow: GET");
-			die();
-		}
-		if(file_exists($filename)){
-			header("Content-type: application/pdf");
-			header("Content-disposition:attachment; filename=".basename($filename).".pdf");
-			echo file_get_contents($filename);
+		if($method == 'GET'){
+			if(file_exists($filename)){
+				header("Content-type: application/pdf");
+				header("Content-disposition:attachment; filename=".basename($filename).".pdf");
+				echo file_get_contents($filename);
+			} else {
+				http_response_code(404);
+			}
+		} else if ($method == 'POST'){
+			if($_SERVER['CONTENT_TYPE'] == 'application/json'){
+				$data = json_decode(file_get_contents('php://input'));
+				
+				$pdf = new PDFBuilder($data);
+				if($pdf && $pdf->saveTo($filename)){
+					http_response_code(201);
+					header("Content-type: application/json");
+					echo json_encode(array("url"=>$request[1]));
+				} else {
+					header("Content-type: application/json");
+					echo json_encode(array("success"=>false));
+				}
+			} else {
+				http_response_code(415);
+			}
 		} else {
-			http_response_code(404);
+			http_response_code(405);
+			header("Allow: GET POST");
 		}
 	}
 	
 	private function getRestRequest(){
-		$relavantURI = $_SERVER['PATH_INFO'];
+		$relavantURI = $_SERVER['PATH_INFO'];//str_replace($_SERVER['SCRIPT_NAME'],'',$_SERVER['REQUEST_URI']);
 		$relavantURI = trim($relavantURI,' /');
 		$requestData = explode('/',$relavantURI);
 		return $requestData;
