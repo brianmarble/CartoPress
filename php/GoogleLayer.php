2716601e915960c9decc673580b086b3947fd311
Alex Wells <1walexw@gmail.com>
2014-01-25 14:40:03 -0700

    Google layer auto calculate zoom

--- php/GoogleLayer.php
+++ php/GoogleLayer.php
@@ -4,59 +4,72 @@ class GoogleLayer extends Layer {
 
 	private $filename;
 	private $pageLayout;
-	
+
 	public function __construct($layer,$pageLayout,$bounds){
 		parent::__construct($layer);
 		$this->pageLayout = $pageLayout;
-		
+
+
 		$tileSize = 256;
-				
+
+		$mapPixelWidth = $this->pageLayout->getMapWidth('pixel');
+
+		$targetMapTileWidth = floor($mapPixelWidth / $tileSize / 2) + 1;
+
+
+		$zoom = -1;
+		do {
+			$zoom++;
+			$zoomTileWidth = $this->getMapTileWidth($layer->lonLatBounds,$zoom);
+		}while($zoomTileWidth < $targetMapTileWidth);
+
+
 		// get the tile number of the top left
 		$topLeftTileNumbers = $this->getTileNumbersFromLonLat(
 			$layer->lonLatBounds->left,
-			$layer->lonLatBounds->top, 
-			$layer->zoom
+			$layer->lonLatBounds->top,
+			$zoom
 		);
 		$topLeftTileX = floor($topLeftTileNumbers['x']);
 		$topLeftTileY = floor($topLeftTileNumbers['y']);
-		
+
 		$leftOffset = round(($topLeftTileNumbers['x'] - floor($topLeftTileNumbers['x'])) * $tileSize);
 		$topOffset = round(($topLeftTileNumbers['y'] - floor($topLeftTileNumbers['y'])) * $tileSize);
-		
-		
+
+
 		// get the tile number of the bottom right
 		$bottomRightTileNumbers = $this->getTileNumbersFromLonLat(
 			$layer->lonLatBounds->right,
-			$layer->lonLatBounds->bottom, 
-			$layer->zoom
+			$layer->lonLatBounds->bottom,
+			$zoom
 		);
 		$bottomRightTileX = floor($bottomRightTileNumbers['x']);
 		$bottomRightTileY = floor($bottomRightTileNumbers['y']);
-		
+
 		$rightOffset = $tileSize - round(($bottomRightTileNumbers['x'] - floor($bottomRightTileNumbers['x'])) * $tileSize);
 		$bottomOffset = $tileSize - round(($bottomRightTileNumbers['y'] - floor($bottomRightTileNumbers['y'])) * $tileSize);
-		
+
 		$imageDealer = new ImageDealer("http://maps.googleapis.com/maps/api/staticmap",(object)array(
 			'sensor' => 'false',
 			'size' => $tileSize.'x'.$tileSize,
 			'maptype' => 'roadmap',
 			'format' => 'roadmap',
-			'zoom' => $layer->zoom
+			'zoom' => $zoom
 		));
-		
+
 		$images = array();
-		
+
 		// for each tile
-		
+
 		for($y = $topLeftTileY; $y <= $bottomRightTileY; $y++){
 			for($x = $topLeftTileX; $x <= $bottomRightTileX; $x++){
-				$center = $this->getLonLatFromTileNumbers($x + .5, $y + .5, $layer->zoom);
+				$center = $this->getLonLatFromTileNumbers($x + .5, $y + .5, $zoom);
 				$images[] = $imageDealer->addImage(array(
 					'center' => $center['lat'].','.$center['lon']
 				));
 			}
 		}
-		
+
 		$this->images = $images;
 		$imageDealer->getImages();
 		$this->grid = (object)array(
@@ -70,23 +83,23 @@ class GoogleLayer extends Layer {
 	}
 
 	function drawLayer($pdf){
-				
+
 		$filename = $this->combineTiles();
-		
+
 		$cfg = Config::getInstance();
 		$pageLayout = $this->pageLayout;
 		$pdf->Image($filename,$cfg->margin,$cfg->margin+$cfg->headerSize,$pageLayout->getMapWidth('in'),$pageLayout->getMapHeight('in'));
-		
+
 	}
-	
+
 	function combineTiles(){
 		$grid = $this->grid;
-			
+
 		$im = new Imagick();
 		$nextImage = 0;
-						
+
 		for($row = 0; $row < $grid->rowCount; $row++){
-			$imRow = new Imagick(); 
+			$imRow = new Imagick();
 			for($col = 0; $col < $grid->colCount; $col++)
 			{
 				$imRow->readimage($this->images[$nextImage++]);
@@ -96,18 +109,18 @@ class GoogleLayer extends Layer {
 			$im->addimage($imRow);
 		}
 		$im->resetiterator();
-		
+
 		$im2 = $im->appendimages(true);
-				
+
 		$im2->cropimage($grid->width, $grid->height, $grid->leftOffset, $grid->topOffset);
 
 		//$filename = "/tmp/tmp.png"; //ImageDealer::getFilename();
 		$filename = ImageDealer::getFilename();
-		
+
 		$im2->writeimage($filename);
 		return $filename;
 	}
-	
+
 	function getTileNumbersFromLonLat($lon, $lat, $zoom){
 		// based on http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
 		$n = pow(2,$zoom);
@@ -116,8 +129,8 @@ class GoogleLayer extends Layer {
 			'x' => $n * (($lon + 180) / 360),
 			'y' => $n * (1 - (log(tan($latRad) + (1 / cos($latRad))) / pi())) / 2
 		);
-	} 
-	
+	}
+
 	function getLonLatFromTileNumbers($x,$y,$zoom){
 		// based on http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
 		$n = pow(2,$zoom);
@@ -128,6 +141,12 @@ class GoogleLayer extends Layer {
 			'lon' => $lonDeg,
 			'lat' => $latDeg
 		);
+	}
+
+	function getMapTileWidth($bounds,$zoom){
+		$left = floor($this->getTileNumbersFromLonLat($bounds->left,$bounds->top,$zoom)['x']);
+		$right = floor($this->getTileNumbersFromLonLat($bounds->right,$bounds->top,$zoom)['x']);
+		return $right - $left + 1;
 	}
 }
 
